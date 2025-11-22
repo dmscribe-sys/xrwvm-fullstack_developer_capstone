962ab6fa0c70e4344806f733f1c15c024fcab59b
Daniel Scribe <dmscribe@gmail.com>
2025-11-22 17:59:53 -0600

    Update STATICFILES_DIRS comment in settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -143,7 +143,7 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# DMSCRIBE UPDATE 
+# DMSCRIBE UPDATE
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/static'),
     os.path.join(BASE_DIR, 'frontend/build'),
